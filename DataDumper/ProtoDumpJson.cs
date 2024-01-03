ad3c90fe5702daa271b057aa33a2aaeea6914c82
Velociraptor115 <Velociraptor115@users.noreply.github.com>
2024-01-03 16:35:29 +0530

    Read from multiple config files in config directory

--- DataDumper/ProtoDumpJson.cs
+++ DataDumper/ProtoDumpJson.cs
@@ -19,12 +19,37 @@ public class ProtoDumpJson
       TypeInfoResolverChain = { CustomJsonSerializerContext.Default }
     };
 
-    var customSerializerConfig = File.ReadAllText($"{jsonDumpDir}/config.json");
-    var customSerializers = JsonSerializer.Deserialize<Dictionary<string, CustomJsonSerializer>>(customSerializerConfig, serializerOpts);
+    var allInOneConfigFile = $"{jsonDumpDir}/config.json";
+    var configDir = $"{jsonDumpDir}/config";
+
+    Dictionary<string, CustomJsonSerializer> customSerializers = new();
+
+    if (File.Exists(allInOneConfigFile))
+    {
+      var aioSerializerConfigs =  JsonSerializer.Deserialize<Dictionary<string, CustomJsonSerializer>>(File.ReadAllText(allInOneConfigFile), serializerOpts);
+      foreach (var kvp in aioSerializerConfigs)
+      {
+        customSerializers.Add(kvp.Key, kvp.Value);
+      }
+    }
+    
+    if (Directory.Exists(configDir))
+    {
+      var configFiles = Directory.GetFiles(configDir, "*.json");
+      foreach (var configFile in configFiles)
+      {
+        var key = Path.GetFileNameWithoutExtension(configFile);
+        var serializerConfig = JsonSerializer.Deserialize<CustomJsonSerializer>(File.ReadAllText(configFile), serializerOpts);
+        customSerializers.Add(key, serializerConfig);
+      }
+    }
+    
+    var dumpFolder = $"{jsonDumpDir}/dumped";
+    Directory.CreateDirectory(dumpFolder);
 
     foreach (var serializerConfigKvp in customSerializers)
     {
-      using var stream = File.Open($"{jsonDumpDir}/{serializerConfigKvp.Key}.json", FileMode.Create, FileAccess.Write);
+      using var stream = File.Open($"{dumpFolder}/{serializerConfigKvp.Key}.json", FileMode.Create, FileAccess.Write);
       serializerConfigKvp.Value.Serialize(stream, new JsonWriterOptions()
       {
         Indented = true
